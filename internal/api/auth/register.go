b1a7a0bd4af1a37f3f98ddc1908be822c164316f
M1racle <crazyhl@163.com>
2023-12-23 18:58:05 +0800

    ✨ 增加用户注册，form 获获取以及验证

--- internal/api/auth/register.go
+++ internal/api/auth/register.go
@@ -1,7 +1,30 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+type user struct {
+	Email  string `json:"email" xml:"email" form:"email" validate:"required,min=5,max=20,email"`
+	Pass   string `json:"pass" xml:"pass" form:"pass" validate:"required,min=5,max=20,alphanum"`
+	RePass string `json:"repass" xml:"repass" form:"repass" validate:"required,eqfield=Pass"`
+}
 
 func Register(ctx *fiber.Ctx) error {
+	u := new(user)
+	if err := ctx.BodyParser(u); err != nil {
+		return ctx.JSON(fiber.Map{
+			"status": -1,
+			"error":  err.Error(),
+		})
+	}
+	if errs := validator.New().Struct(u); errs != nil {
+		return ctx.JSON(fiber.Map{
+			"status": -2,
+			"error":  errs.Error(),
+		})
+	}
+
 	return nil
 }
--- internal/model/user.go
+++ internal/model/user.go
@@ -2,7 +2,8 @@ package model
 
 type User struct {
 	BaseModel
-	Name  string `gorm:"not null"`
-	Email string `gorm:"not null;unique"`
-	Cover string `gorm:"not null"`
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"not null;unique"`
+	Cover    string `gorm:"not null"`
+	Password string `gorm:"not null;"`
 }
