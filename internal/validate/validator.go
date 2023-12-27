dfbcfa3ae06eaf79cef085afaff7f8217dd0a1e5
M1racle <crazyhl@163.com>
2023-12-27 11:35:25 +0800

    ✨ 覆写验证 eqField 翻译信息

--- internal/validate/validator.go
+++ internal/validate/validator.go
@@ -1,7 +1,13 @@
 package validate
 
-import "github.com/gookit/validate/locales/zhcn"
+import (
+	"github.com/gookit/validate"
+	"github.com/gookit/validate/locales/zhcn"
+)
 
 func Init() {
 	zhcn.RegisterGlobal()
+	validate.AddGlobalMessages(map[string]string{
+		"eqField": "{field} 字段值必须和 %s 字段值相同",
+	})
 }
