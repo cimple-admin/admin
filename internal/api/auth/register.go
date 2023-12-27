d38727b185e9bb59025949a70628e0ac14446dc6
M1racle <crazyhl@163.com>
2023-12-27 14:59:17 +0800

    ✨ 完善注册，以及基础模型定义

--- internal/api/auth/register.go
+++ internal/api/auth/register.go
@@ -1,32 +1,54 @@
 package auth
 
 import (
+	"github.com/cimple-admin/admin/internal/model"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gookit/validate"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type user struct {
-	Email  string `json:"email" xml:"email" form:"email" validate:"required|email" label:"邮箱"`
-	Pass   string `json:"pass" xml:"pass" form:"pass" validate:"required|minLength:5|maxLength:20|password" label:"密码"`
-	RePass string `json:"repass" xml:"repass" form:"repass" validate:"required|eq_field:Pass" label:"确认密码"`
+	Email           string `json:"email" xml:"email" form:"email" validate:"required|email" label:"邮箱"`
+	Password        string `json:"password" xml:"password" form:"password" validate:"required|minLength:5|maxLength:20|password" label:"密码"`
+	ConfirmPassword string `json:"confirm_password" xml:"confirm_password" form:"confirm_password" validate:"required|eq_field:Password" label:"确认密码"`
 }
 
 func Register(ctx *fiber.Ctx) error {
 	u := new(user)
 	if err := ctx.BodyParser(u); err != nil {
 		return ctx.JSON(fiber.Map{
-			"status": -1,
-			"error":  err.Error(),
+			"status":  -1,
+			"message": err.Error(),
 		})
 	}
 
 	v := validate.Struct(u)
 	if !v.Validate() {
 		ctx.JSON(fiber.Map{
-			"status": -2,
-			"error":  v.Errors.One(),
+			"status":  -2,
+			"message": v.Errors.One(),
 		})
 	}
 
-	return nil
+	password, err := bcrypt.GenerateFromPassword([]byte(u.Password), 8)
+	if err != nil {
+		ctx.JSON(fiber.Map{
+			"status":  -3,
+			"message": err,
+		})
+	}
+	user := model.User{Email: u.Email, Password: string(password), Name: u.Email}
+	result := model.DB.Create(&user)
+
+	if result.Error != nil {
+		ctx.JSON(fiber.Map{
+			"status":  -4,
+			"message": result.Error,
+		})
+	}
+
+	return ctx.JSON(fiber.Map{
+		"status":  0,
+		"message": "注册成功",
+	})
 }
--- internal/model/base.go
+++ internal/model/base.go
@@ -2,6 +2,6 @@ package model
 
 type BaseModel struct {
 	ID       uint
-	CreateAt uint `gorm:"autoCreateTime;default:0;not null"`
-	UpdateAt uint `gorm:"autoUpdateTime;default:0;not null"`
+	CreateAt uint `gorm:"autoCreateTime;not null"`
+	UpdateAt uint `gorm:"autoUpdateTime;not null"`
 }
